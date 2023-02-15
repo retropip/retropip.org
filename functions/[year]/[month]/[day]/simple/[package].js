e1d396d81947c0de9030d1a8acace7774331b86e
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2023-02-15 19:34:24 +0100

    feature: implement package endpoint

--- functions/[year]/[month]/[day]/simple/[package].js
+++ functions/[year]/[month]/[day]/simple/[package].js
@@ -1,6 +1,41 @@
+const JSON_URL = 'https://pypi.org/pypi/{package}/json';
+
+const PACKAGE_HTML = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Links for {package}</title>
+  </head>
+  <body>
+    <h1>Links for {package}</h1>
+{links}
+  </body>
+</html>
+`;
+
+function parseIso(dt) {
+  return new Date(dt);
+}
+
 /*
  * 
  */
-export function onRequest(context) {
-  return new Response(context.params.month)
+export async function onRequestGet(context) {
+  const cutoff = Date(`${context.param.year}-${context.param.month}-${context.param.day}`);
+  const packageIndex = await fetch(JSON_URL.replace('{package}', context.param.package)).then(response => response.json());
+  let releaseLinks = '';
+  for (const release of Object.values(packageIndex.releases)) {
+    for (const file of release) {
+      const releaseDate = parseIso(file.upload_time);
+      if (releaseDate < cutoff) {
+        if (file.requires_python === null) {
+          releaseLinks += `    <a href="${file.url}#sha256=${file.digests.sha256}">${file.filename}</a><br/>\n`;
+        } else {
+          const rp = file.requires_python.replace('>', '&gt;');
+          releaseLinks += `    <a href="${file.url}#sha256=${file.digests.sha256}" data-requires-python="${rp}">${file.filename}</a><br/>\n`;
+        }
+      }
+    }
+  }
+  return new Response(PACKAGE_HTML.replace('{package}', package).replace('{links}', releaseLinks));
 }
