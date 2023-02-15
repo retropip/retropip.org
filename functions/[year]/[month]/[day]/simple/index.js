f6a22afbe6b798da4e4e8a5625556a4e05073197
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2023-02-15 19:02:43 +0100

    feature: serve as html

--- functions/[year]/[month]/[day]/simple/index.js
+++ functions/[year]/[month]/[day]/simple/index.js
@@ -5,7 +5,10 @@ export async function onRequestGet(context) {
   // Request pypi's /simple/ package index
   const response = await fetch("https://pypi.org/simple/");
   if (response.ok) {
-    return new Response(await response.text());
+    let headers = new Headers({
+        'Content-Type': 'text/html'
+    });
+    return new Response(await response.text(), { headers });
   }
 }
 
