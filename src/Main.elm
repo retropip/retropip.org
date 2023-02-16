6ef67fb6e80e07690605fc8b0a5d386030eed7f8
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2023-02-16 18:34:51 +0100

    feature: elm element app inside old html

--- src/Main.elm
+++ src/Main.elm
@@ -6,18 +6,24 @@ import Html.Events exposing (onClick)
 
 main : Program () Int Msg
 main =
-  Browser.sandbox { init = 0, update = update, view = view }
+  Browser.element { init = init, update = update, subscriptions = subscriptions, view = view }
 
 type Msg = Increment | Decrement
 
-update : Msg -> number -> number
+init : () -> (Int, Cmd Msg)
+init _ = (0, Cmd.none)
+
+update : Msg -> number -> (number, Cmd Msg)
 update msg model =
   case msg of
     Increment ->
-      model + 1
+      (model + 1, Cmd.none)
 
     Decrement ->
-      model - 1
+      (model - 1, Cmd.none)
+
+subscriptions : Int -> Sub Msg
+subscriptions _ = Sub.none
 
 view : Int -> Html Msg
 view model =
