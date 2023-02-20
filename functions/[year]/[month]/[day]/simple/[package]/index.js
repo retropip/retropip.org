1b5564986d4f4e73feb4cbd658f8d7a6c9b64595
César Gallego Rodríguez <gallego.cesar@gmail.com>
2023-02-20 19:22:30 +0100

    refactor: js to heaven from hell

--- functions/[year]/[month]/[day]/simple/[package]/index.js
+++ functions/[year]/[month]/[day]/simple/[package]/index.js
@@ -1,50 +1,78 @@
-const JSON_URL = 'https://pypi.org/pypi/{pythonpackage}/json';
+const jsonUrl = (pack) => `https://pypi.org/pypi/${pack}/json`;
 
-const PACKAGE_HTML = `
+const html = (pack, links) => `
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Links for {pythonpackage}</title>
+    <meta charset="utf-8">
+    <title>Links for ${pack}</title>
   </head>
   <body>
-    <h1>Links for {pythonpackage}</h1>
-{links}
+    <h1>Links for ${pack}</h1>
+    ${links.map((link) => `<!-- ${link.date} -->\n<a href="${link.url}#${link.sha256}">${link.name}</a>`).join('\n')}
   </body>
 </html>
 `;
 
-function parseIso(dt) {
-  return new Date(dt);
+function generateLinks(json) {
+  return Object.values(json.releases)
+    .flatMap((r) => {
+      return r.map((release) => {
+        return {
+          name: release.filename,
+          url: release.url,
+          sha256: release.digests.sha256,
+          date: new Date(release.upload_time),
+        };
+      });
+  });
 }
 
-/*
- * 
- */
+function filterLinks(cutoff) {
+  return (links) => {
+    return links.filter((link) => link.date < cutoff);
+  };
+}
+
+// to run locally comment out export keyword
 export async function onRequestGet(context) {
-  const cutoff = parseIso(`${context.params.year}-${context.params.month}-${context.params.day}T00:00:00`);
-  console.log(cutoff);
-  const response = await fetch(JSON_URL.replace('{pythonpackage}', context.params.package));
-  if (!response.ok) {
-    return new Response("not found", { status: 404 });
+  const pack = context.params.package;
+  const year = context.params.year;
+  const month = context.params.month;
+  const day = context.params.day;
+
+  const cutoff = new Date(year, month - 1, day);
+  //TODO: validate if the date is valid
+
+  const res = await fetch(jsonUrl(pack));
+  if (!res.ok) {
+    const stat = res.status;
+    return new Response(`Error fetching json: ${stat}`, { status: stat });
   }
-  const packageIndex = await response.json();
-  let releaseLinks = '';
-  for (const release of Object.values(packageIndex.releases)) {
-    for (const file of release) {
-      const releaseDate = parseIso(file.upload_time);
-      console.log(releaseDate);
-      if (releaseDate < cutoff) {
-        if (file.requires_python === null) {
-          releaseLinks += `    <a href="${file.url}#sha256=${file.digests.sha256}">${file.filename}</a><br/>\n`;
-        } else {
-          const rp = file.requires_python.replace('>', '&gt;');
-          releaseLinks += `    <a href="${file.url}#sha256=${file.digests.sha256}" data-requires-python="${rp}">${file.filename}</a><br/>\n`;
-        }
-      }
-    }
-  }
-  let headers = new Headers({
-      'Content-Type': 'text/html'
+
+  const json = await res.json();
+  const links = generateLinks(json).filter(filterLinks(cutoff));
+
+  return new Response(html(pack, links), {
+    headers: {
+      'content-type': 'text/html',
+    },
   });
-  return new Response(PACKAGE_HTML.replaceAll('{pythonpackage}', context.params.package).replace('{links}', releaseLinks), { headers });
+}
+
+// to run locally and testing pruposes
+if (require.main === module) {
+  date = new Date(process.argv[3]);
+  fetch(jsonUrl(process.argv[2]))
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      throw new Error('Error fetching json');
+    })
+    .then(generateLinks)
+    .then(filterLinks(date))
+    .then((links) => {
+      console.log(html(process.argv[2], links));
+    });
 }
